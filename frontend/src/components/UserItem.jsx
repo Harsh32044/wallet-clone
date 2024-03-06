93b0fcdbed8ffd0a41ce1c7e4001c181e31ca9ff
Harsh Srivastav <hsrivastav45@gmail.com>
2024-03-07 04:33:40 +0530

    Adding modal for sending money

--- frontend/src/components/UserItem.jsx
+++ frontend/src/components/UserItem.jsx
@@ -1,6 +1,24 @@
 import { NameInitialsAvatar } from "react-name-initials-avatar";
+import Modal from "react-modal";
+import { useState } from "react";
 
+Modal.setAppElement("#root");
 export default function UserItem({ user }) {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const customStyles = {
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+      backgroundColor: "white",
+      width: "400px",
+    },
+  };
+
   return (
     <div className="flex justify-between items-center py-4 rounded-md cursor-pointer px-4 hover:bg-gray-200">
       <div className="flex items-center">
@@ -14,9 +32,38 @@ export default function UserItem({ user }) {
           {user.firstName} {user.lastName}
         </div>
       </div>
-      <button className="bg-gray-900 text-gray-200 hover:bg-gray-700 rounded-md p-2">
+      <button
+        className="bg-blue-500 text-gray-100 hover:bg-blue-400 rounded-md p-2"
+        onClick={() => setModalOpen(true)}
+      >
         Send Money
       </button>
+      <Modal
+        isOpen={modalOpen}
+        onRequestClose={() => setModalOpen(false)}
+        style={customStyles}
+        bodyOpenClassName="modal-open"
+        className="shadow-md absolute p-6"
+      >
+        <div className="flex flex-col">
+          <div className="font-bold text-2xl text-center">Send Money</div>
+          <div className="flex items-center my-4">
+            <NameInitialsAvatar
+              bgColor="whitesmoke"
+              textColor="gray"
+              borderWidth="0"
+              name={`${user.firstName} ${user.lastName}`}
+            />
+            <div className="font-semibold ml-4">
+              {user.firstName} {user.lastName}
+            </div>
+          </div>
+          <label htmlFor="txnAmount">Amount (in Rs.)</label>
+          <input type="text" name="txnAmount" id="txnAmount" 
+          placeholder="Enter Amount" className="p-2 my-4 border-2"/>
+          <button className="bg-blue-500 text-gray-50 rounded-md h-10 hover:bg-blue-400">Initiate Transfer</button>
+        </div>
+      </Modal>
     </div>
   );
 }
